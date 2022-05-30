4fc032bed1bd05e5a40dd41129f62d0761cb3614
Poga Kuofie <mail@pogakuofie.com>
2022-05-30 12:50:10 +0000

    feat: login logic and favicon, icon change

--- pages/login.tsx
+++ pages/login.tsx
@@ -11,20 +11,44 @@ import PageContainer from "../components/common/page";
 const Login: NextPage = () => {
   useEffect(() => {}, []);
 
-  const { recaptchaVerifier } = useAuth();
+  const { recaptchaVerifier, setPhoneNumber, setOTPCode, login } = useAuth();
 
   return (
     <PageContainer>
       <StyledText>Hi, Login</StyledText>
       <div style={{}} id='sign-in-button'></div>
       <div id='recaptcha-container'></div>
+      {/* <input
+        type={"text"}
+        maxLength={10}
+        onChange={({ target }) => {
+          const { value } = target;
+
+          setPhoneNumber(value);
+        }}
+      />
+      <input
+        maxLength={6}
+        type={"text"}
+        onChange={({ target }) => {
+          const { value } = target;
+          setOTPCode(value);
+        }}
+      />
       <button
         onClick={() => {
           recaptchaVerifier();
         }}
       >
-        Start
+        Send
       </button>
+      <button
+        onClick={() => {
+          login();
+        }}
+      >
+        Verify
+      </button> */}
     </PageContainer>
   );
 };
