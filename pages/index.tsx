b4f3626c539b2294a31f70a8ad71a5c48a37c358
Poga Kuofie <mail@pogakuofie.com>
2022-06-26 12:05:37 +0000

    chore: responsiveness code clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,44 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 // components
-import { Box, Buttton, Logo, StyledText } from "../components/common/basic";
+import { Box, Button, Logo, StyledText } from "../components/common/basic";
 import { PageContainer, Pager } from "../components/common/page";
 
 // config
 
 const Home: NextPage = () => {
+  const [isMobile, setIsMobile] = useState<boolean>();
+  const [mobileCarouselState, setMobileCarouselState] = useState<number>(0);
+
+  useEffect(() => {
+    setMedia();
+
+    window.addEventListener("resize", setMedia);
+  }, []);
+
+  const setMedia = () => {
+    let { matches } = window.matchMedia("(max-width: 768px)");
+
+    setIsMobile(matches);
+  };
+
+  const handleStartCTA = () => {
+    let { matches } = window.matchMedia("(max-width: 768px)");
+
+    if (matches) {
+    }
+  };
+
   return (
     <PageContainer padding={0}>
       <Pager
-        style={{ flexDirection: "row-reverse" }}
+        flexDirection={"column"}
         backgroundImage={"../assets/images/background_logo.svg"}
+        padding={16}
       >
-        <Nav>
+        <Nav display={isMobile ? "none" : "flex"} padding={0}>
           <Logo height={45} width={45} src='../assets/images/logo.svg' />
           <Menu padding={0}>
             <StyledText
@@ -24,6 +48,7 @@ const Home: NextPage = () => {
               }}
               marginLeft={14}
               size={18}
+              color='#3C0F95'
             >
               Home
             </StyledText>
@@ -34,6 +59,7 @@ const Home: NextPage = () => {
               }}
               marginLeft={14}
               size={18}
+              color='#3C0F95'
             >
               About
             </StyledText>
@@ -44,24 +70,31 @@ const Home: NextPage = () => {
               }}
               marginLeft={14}
               size={18}
+              color='#3C0F95'
             >
               Sign In
             </StyledText>
           </Menu>
         </Nav>
         <Box
-          style={{ flexDirection: "column" }}
+          flex={1}
+          flexDirection='column'
           padding={50}
           justifyContent={"center"}
+          align-items={"center"}
         >
-          <MobileLogo marginBottom={15} padding={0} justifyContent={"center"}>
-            <Logo height={45} width={45} src='../assets/images/logo.svg' />
-          </MobileLogo>
+          <Logo
+            marginBottom={20}
+            display={isMobile ? "flex" : "none"}
+            alignSelf={"center"}
+            height={45}
+            width={45}
+            src='../assets/images/logo.svg'
+          />
           <StyledText
             weight={500}
-            textAlign={"center"}
-            size={54}
-            sizeDiffMobile={22}
+            align={"center"}
+            size={isMobile ? 34 : 54}
             color={"#3C0F95"}
             margin={0}
           >
@@ -69,26 +102,54 @@ const Home: NextPage = () => {
             <br /> Always.
           </StyledText>
         </Box>
-        <Box
-          style={{ flex: 1, alignItems: "flex-end" }}
-          justifyContent='center'
-        >
-          <Buttton
+        <Box align-items={"center"} justifyContent='center'>
+          <Button
             height={45}
             width={350}
             backgroundColor={"#34C4FA"}
             widthDiffMobile={100}
             cursor={"pointer"}
+            borderRadius={5}
+            justifyContent={"center"}
+            onClick={() => {
+              handleStartCTA();
+            }}
           >
             <StyledText
               cursor={"pointer"}
-              size={18}
-              sizeDiffMobile={2}
-              color='#ffffff'
+              size={isMobile ? 16 : 18}
+              color='#3C0F95'
+              alignSelf='center'
+              weight={400}
+              userSelect={"none"}
             >
               GET STARTED
             </StyledText>
-          </Buttton>
+          </Button>
+        </Box>
+      </Pager>
+      <Pager
+        display={isMobile ? "none" : "flex"}
+        padding={0}
+        flexDirection={"row"}
+      >
+        <Box flexDirection='column'>
+          <StyledText size={44} height={52}>
+            Clean clothes for elegance and confidence.
+          </StyledText>
+          <StyledText height={46}>
+            We work to make your clothes clean and ready, just the way you want
+            them.
+          </StyledText>
+        </Box>
+        <Box flexDirection='column' padding={0} backgroundColor='#34C4FA'>
+          <StyledText height={56}>
+            Clean clothes for elegance and confidence.
+          </StyledText>
+          <StyledText height={46}>
+            We work to make your clothes clean and ready, just the way you want
+            them.
+          </StyledText>
         </Box>
       </Pager>
     </PageContainer>
@@ -99,52 +160,7 @@ export default Home;
 
 const Menu = styled(Box)``;
 
-const Nav = styled.div`
-  display: flex;
+const Nav = styled(Box)`
   justify-content: space-between;
   align-items: center;
-
-  @media only screen and (max-width: 595px) {
-    display: none;
-  }
-
-  @media only screen and (min-width: 600px) {
-    display: none;
-  }
-
-  @media only screen and (min-width: 768px) {
-    display: none;
-  }
-
-  @media only screen and (min-width: 1100px) {
-    display: flex;
-  }
-
-  @media only screen and (min-width: 1400px) {
-    display: flex;
-  }
-`;
-
-const MobileLogo = styled(Box)<{ marginBottom?: number }>`
-  margin-bottom: ${({ marginBottom }) => `${marginBottom}px`};
-
-  @media only screen and (max-width: 595px) {
-    display: flex;
-  }
-
-  @media only screen and (min-width: 600px) {
-    display: flex;
-  }
-
-  @media only screen and (min-width: 768px) {
-    display: flex;
-  }
-
-  @media only screen and (min-width: 1100px) {
-    display: none;
-  }
-
-  @media only screen and (min-width: 1400px) {
-    display: none;
-  }
 `;
